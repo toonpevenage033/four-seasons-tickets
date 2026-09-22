9dbac9674fbe1ea2dc68ceb2d97de986ba920de0
toonpevenage033 <toon.pevenage@icloud.com>
2026-09-22 16:36:58 +0200

    tijdelijk testen disk

--- server/pricing.js
+++ server/pricing.js
@@ -17,7 +17,7 @@ const TIERS = [
     id: "earlybird",
     label: "Early Bird",
     priceCents: 1050,
-    start: "2026-09-24T15:00:00",
+    start: "2020-01-01T00:00:00",
     end: "2026-10-01T00:00:00",
   },
   {
